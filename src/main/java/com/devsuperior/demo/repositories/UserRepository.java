3baafe377b2fd6883fea1cea24211896e6708686
Tawanna <thol_16@hotmail.com>
2024-01-16 20:58:42 -0300

    CityRepository, EventRepository and UserRepository created

---
+++ src/main/java/com/devsuperior/demo/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.demo.repositories;
+
+import com.devsuperior.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
